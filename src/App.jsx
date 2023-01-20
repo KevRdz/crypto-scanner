00ae736b88c744310f2a7d20f157bf94e2c64985
Kevin <kevin.rodriguez678@gmail.com>
2023-01-20 15:32:59 -0500

    create coinitem and coin component and passed props

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,27 @@
-import { Route, Routes } from 'react-router-dom';
+// import { Route, Routes } from 'react-router-dom';
+import React, {useState, useEffect} from 'react';
 import './App.css';
-import Header from './components/Header/Header';
-import CoinPage from './pages/CoinPage/CoinPage';
-import HomePage from './pages/HomePage/HomePage';
+import axios from 'axios';
+import Coins from './components/Coins/Coins';
 
 function App() {
+
+  const [coins, setCoins] = useState([])
+
+  const url = 'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false'
+
+  useEffect(() => {
+    axios.get(url).then((response) => {
+      setCoins(response.data)
+      console.log(response.data[0])
+    }).catch((error) => {
+      console.log(error)
+    })
+  } , [])
+
   return (
     <>
-      <Header />
-      <Routes>
-        <Route path='/' element={<HomePage />} />
-        <Route path='/coins:id' element={<CoinPage />} />
-      </Routes>
+      <Coins coins={coins}/>
     </>
   );
 }
